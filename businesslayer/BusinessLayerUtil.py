04809ceb19b3b9e184fe4028f5adc4e2b673d823
Ekta Sharma <ekta.sharma3@gmail.com>
2020-02-25 12:20:12 -0600

    added more code

--- businesslayer/BusinessLayerUtil.py
+++ businesslayer/BusinessLayerUtil.py
@@ -11,4 +11,10 @@ class BusinessLayer:
 
 		imgScrapper = ScrapperImage  # creating an object of ScarpperImage class (data layer)
 		url = imgScrapper.createImageUrl(keyword)
-		rawHtml = imgScrapper.scrape_html_data(url, header)
+		soup = imgScrapper.scrape_html_data(url, header)
+
+		img_url_list = imgScrapper.get_img_url_list(soup)
+
+		img_list = imgScrapper.download_images(img_url_list, keyword, header)
+
+		return img_list
--- scrapperImage/ScrapperImage.py
+++ scrapperImage/ScrapperImage.py
@@ -20,7 +20,80 @@ class ScrapperImage:
 	def scrape_html_data(url, header):
 		request = urllib.request.Request(url, headers=header)
 		response = urllib.request.urlopen(request)
-		response_data = response.read()
-		html = Beautifulsoup(response_data, "html.parser")
-		return html
+		html = response.read()
+		soup = Beautifulsoup(response_data, "html.parser")
+		return soup
+
+	# extracting image link and ext
+	def get_img_url_list(soup):
+		img_url_list = []
+
+		for item in soup.find_all("div", class_="rg_meta"):
+			link = json.loads(item.text)["ou"]
+			ext = json.loads(item.text)["ity"]
+			img_url_list.append((link, ext))
+
+		print(f"There are total {len(img_url_list)} images.")
+		return img_url_list
+
+
+	def download_images(img_url_list, img_name, header):
+		master_img_list = []
+		img_files = []
+		img_types = []
+		count = 0
+		img_counter = 0
+
+		for i, (link, type) in enumerate(img_url_list):
+
+			try:
+				if count > 10:
+					break
+				else:
+					count += 1
+					req = urllib.request.Request(link, headers=header)
+					try:
+						urllib.request.urlretrieve(link, "./static/" + img_name + str(img_counter) + ".jfif")
+						img_counter += 1
+					except Exception as e:
+						print("Download Failed ", e)
+						img_counter += 1
+
+					response = urllib.request.urlopen(req)
+					raw_img = response.read()
+
+					img_files.append(raw_img)
+					img_types.append(type)
+
+			except Exception as e:
+				print("Failed to download ", link)
+				print(e)
+				count += 1
+
+		master_img_list.append(img_files)
+		master_img_list.append(img_types)
+
+		return img_list
+
+
+		def delete_downloaded_images(self, list_of_images):
+			for self.image in list_of_images:
+				try:
+					os.remove("./static"+self.image)
+				except Exception as e:
+					print("Error in deleting ", e)
+			return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
 
